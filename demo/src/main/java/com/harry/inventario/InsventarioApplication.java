b615d676e5e10baefa4e452fe4483499142727e8
Hector Vargas <hector.vargas.torrejon@gmail.com>
2023-02-20 19:56:17 -0300

    proyecto base control de inventario

---
+++ demo/src/main/java/com/harry/inventario/InsventarioApplication.java
@@ -0,0 +1,13 @@
+package com.harry.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsventarioApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/harry/inventario/InsventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harry.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
